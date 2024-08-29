030aa4064f304780cf1170aa22aac7637a17eda9
Luke-the-Husker <lmcgrew3@huskers.unl.edu>
2024-08-29 08:01:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ *         LMcGrew3@unl.edu
+ * Date: 2024/08/29
+ *
+ * A simple hello in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Luke McGrew\n");
+
+  return 0;
+}
